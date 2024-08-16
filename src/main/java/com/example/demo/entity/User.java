548821cf1e4ddda871c4f2d72931676840e19cdf
Chamidu Dilshan <chamindu2150@gmail.com>
2024-08-16 12:30:25 +0530

    dependencies

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,4 +1,9 @@
 package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 import javax.persistence.*;
 
